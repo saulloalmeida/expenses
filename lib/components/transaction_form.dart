7a45914cd5313209ee3c22b24f77c1f540389439
Saullo Almeida <saullorhamon@gmail.com>
2020-04-03 06:31:42 -0300

    Melhorias no submit do form

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,47 +1,54 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-    final titleController = TextEditingController();
-    final valueController = TextEditingController();
-    final void Function(String, double) onSubmit;
-    TransactionForm(this.onSubmit);
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+  final void Function(String, double) onSubmit;
+  TransactionForm(this.onSubmit);
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.00;
+    if(title.isEmpty || value <=0){
+      return;
+    }
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(labelText: 'Título'),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      FlatButton(
-                        color: Colors.purple,
-                        textColor: Colors.white,
-                        onPressed: () {
-                          final title = titleController.text;
-                          final value = double.tryParse(valueController.text) ?? 0.00;
-                          onSubmit(title, value);
-                        },
-                        child: Text("Nova Transação"),
-                      ),
-                    ],
-                  )
-                ],
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              onSubmitted: (_)=>{_submitForm()},
+              decoration: InputDecoration(labelText: 'Título'),
+            ),
+            TextField(
+              controller: valueController,
+              onSubmitted: (_)=>{_submitForm()},
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
               ),
             ),
-          );
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                  onPressed: _submitForm,
+                  child: Text("Nova Transação"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
